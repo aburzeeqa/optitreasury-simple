735d945d9adface80f9b441e974c3f738cd93fd8
aburzeeqa <abu.rzeeqa@gmail.com>
2025-05-16 08:53:18 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, 
+    static_folder='src/static',
+    template_folder='src/templates')
 
 @app.route('/')
 def home():
-    return "مرحباً بك في تطبيق OptiTreasury! سيتم إطلاق النسخة الكاملة قريباً."
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
