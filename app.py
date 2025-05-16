d3211e4c5efb053ec5e97a585df9516e4fc9d31a
aburzeeqa <abu.rzeeqa@gmail.com>
2025-05-16 22:52:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
+import opti_db_setup  # هذا السطر الجديد لتشغيل إنشاء الجداول
 
-app = Flask(__name__, 
-    static_folder='src/static',
-    template_folder='src/templates')
+app = Flask(__name__,
+            static_folder='src/static',
+            template_folder='src/templates')
 
 @app.route('/')
 def home():
